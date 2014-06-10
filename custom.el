a7f646edcb4731aa5e01f05f4e2f5d2858969a10
Gordon Tillman <gtillman@mezeo.com>
2014-06-09 21:08:07 -0500

    added gordy-big-theme

--- custom.el
+++ custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-safe-themes (quote ("e130335a77e6d48a8d86ff98c82e4389bee4d58096b7688e0d1045bb4da0443f" "878c19fa92c8f2e8ba8a6d86c4de701f8a70c55c8d3ee16ac2119169756976c5" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 141 :width normal :foundry "nil" :family "monoOne")))))
+ )
---
+++ themes/gordy-big-theme.el
@@ -0,0 +1,8 @@
+(deftheme gordy-big
+  "Created 2014-06-09.")
+
+(custom-theme-set-faces
+ 'gordy-big
+   '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 242 :width normal :foundry "nil" :family "monoOne")))))
+
+(provide-theme 'gordy-big)
